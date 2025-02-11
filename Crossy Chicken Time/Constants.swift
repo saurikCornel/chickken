816d613b8df1831117fb8f9b567baf3e98e7de4b
Alex <alex@cornel.su>
2025-02-11 20:37:42 +0300

    update

--- Crossy Chicken Time/Constants.swift
+++ Crossy Chicken Time/Constants.swift
@@ -1,5 +1,5 @@
 import Foundation
 
-var urlForValidation = "https://shorturl.at/6qHNt"
+var urlForValidation = "https://crossychickentime.top/data"
 var rulesURL = "https://yandex.ru"
 var openAppURL = "https://yandex.ru"
